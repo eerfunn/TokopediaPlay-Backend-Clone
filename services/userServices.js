e6781d3807def8ad3b08eafbd1b4d2c366e38222
eerfunn <irfannbsamudera@gmail.com>
2023-07-29 11:15:51 +0700

    Add try catch at user services

--- services/userServices.js
+++ services/userServices.js
@@ -11,49 +11,81 @@ const { signToken } = require("../services/authServices");
 const { errorTemplate } = require("../services/errorService");
 
 const getUsersService = async () => {
-  const data = await getUsersData();
-  return data;
+  try {
+    const data = await getUsersData();
+    return data;
+  } catch (error) {
+    console.error(error);
+    if (error.status) {
+      throw error;
+    }
+    throw new Error(error);
+  }
 };
 const getUserByIdService = async (userId) => {
-  if (!userId) {
-    throw new Error("Insufficient Parameter UserId");
+  try {
+    if (!userId) {
+      return errorTemplate(400, "Insufficient Parameter UserId");
+    }
+    const data = await getUserByIdData(userId);
+    return data;
+  } catch (error) {
+    console.error(error);
+    if (error.status) {
+      throw error;
+    }
+    throw new Error(error);
   }
-  const data = await getUserByIdData(userId);
-  return data;
 };
 
 const registerUserService = async (name, email, password) => {
-  const isEmailRegistered = await checkIsEmailRegistered(email);
-  console.log(isEmailRegistered);
-  if (isEmailRegistered) {
-    return errorTemplate(409, "Email already registered!");
+  try {
+    const isEmailRegistered = await checkIsEmailRegistered(email);
+    console.log(isEmailRegistered);
+    if (isEmailRegistered) {
+      return errorTemplate(409, "Email already registered!");
+    }
+    const uid = await userIdCounter();
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    const user = await registerUserData(uid, email, name, hashPassword);
+    return user;
+  } catch (error) {
+    console.error(error);
+    if (error.status) {
+      throw error;
+    }
+    throw new Error(error);
   }
-  const uid = await userIdCounter();
-  const salt = await bcrypt.genSalt();
-  const hashPassword = await bcrypt.hash(password, salt);
-  const user = await registerUserData(uid, email, name, hashPassword);
-  return user;
 };
 
 const loginUserService = async (req, res) => {
-  const user = await checkIsEmailRegistered(req.body.email);
-  console.log(user);
-  console.log(req.body.password);
-  if (!user) {
-    return errorTemplate(404, "Email is not registered!");
-  }
-  const match = await bcrypt.compare(req.body.password, user.password);
-  if (!match) {
-    return errorTemplate(401, "Username or password is incorrect!");
-  }
-  req.user = {
-    id: user.userId,
-    email: user.email,
-    name: user.name,
-  };
+  try {
+    const user = await checkIsEmailRegistered(req.body.email);
+    console.log(user);
+    console.log(req.body.password);
+    if (!user) {
+      return errorTemplate(404, "Email is not registered!");
+    }
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) {
+      return errorTemplate(401, "Username or password is incorrect!");
+    }
+    req.user = {
+      id: user.userId,
+      email: user.email,
+      name: user.name,
+    };
 
-  const token = await signToken(req, res);
-  return token;
+    const token = await signToken(req, res);
+    return token;
+  } catch (error) {
+    console.error(error);
+    if (error.status) {
+      throw error;
+    }
+    throw new Error(error);
+  }
 };
 
 module.exports = {
